c866eefdf31c26868a96e5b0f2e2aaa52d407d60
Renato <renatkaitmazov@gmail.com>
2015-10-10 21:34:55 +0300

    Sieve of Eratosthenes
    Finds all the prime numbers from 2 to the upper bound defined by a user

---
+++ Java/Numbers/Sieve of Eratosthenes.java
@@ -0,0 +1,32 @@
+public final class Eratosthenes {
+
+    public static void calculate() {
+        // Get user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter upper bound");
+        int upperBound = scanner.nextInt();
+        if (upperBound < 2) throw new IllegalArgumentException("Number must be greater than two");
+
+        // Create an array of numbers from 2 to the upper bound
+        int[] numbers = new int[upperBound - 1];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 2;
+        }
+
+        // Find prime numbers
+        for (int i = 0; numbers[i] * numbers[i] <= upperBound; i++) {
+            int powIndex = numbers[i] * numbers[i] - 2;
+            if (numbers[i] == 0) continue;
+
+            int notPrimeIndex = powIndex;
+
+            while (notPrimeIndex < numbers.length) {
+                numbers[notPrimeIndex] = 0;
+                notPrimeIndex += numbers[i];
+            }
+        }
+
+        // Print out all the prime numbers found
+        for (int i: numbers) if (i != 0) System.out.format("%d ", i);
+    }
+}
