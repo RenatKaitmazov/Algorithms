2c457aa66912aadb10e5b8a76ef0c18d11ed448c
Renato <renatkaitmazov@gmail.com>
2015-10-22 22:37:13 +0300

    Initial commit
    Sattolo's algorithms for shuffling arrays

---
+++ Java/Sattolo.java
@@ -0,0 +1,17 @@
+public final class Sattolo<T> {
+    public static <T> T[] cycle(T[] arr) {
+        int n = arr.length - 1;
+
+        while (n > 1) {
+            // Generate a random number
+            int rand = (int) (Math.random() * n);
+            
+            // Swap values at according indices
+            final T temp = arr[rand];
+            arr[rand] = arr[n];
+            arr[n--] = temp;
+        }
+
+        return arr;
+    }
+}
